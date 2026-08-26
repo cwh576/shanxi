1777f5648cd9538f9e7f9b3de29f03917060a276
cwenh576-pixel <cwenh576@gmail.com>
2026-08-26 19:58:08 +0800

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,6 @@
+window.SUPABASE_CONFIG = {
+  url: 'https://qivnndtvzpsvzprhurjx.supabase.co',
+  publishableKey: 'sb_publishable_VDWMCmYSREb43z8FTns2Rw_7GqT1Xt-',
+  table: 'dashboard_state',
+  rowId: 'primary'
+};
